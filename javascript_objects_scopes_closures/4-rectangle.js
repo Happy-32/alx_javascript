9b76a0177804c23d6d98e72730ce60e459c71b54
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-10-21 02:51:33 +0000

    Objects in Javascript

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h) { 
+        if (w>0 && h>0){
+            this.width = w;
+            this.height = h;
+        }
+        else if (w <= 0 || h <= 0){
+            
+        }
+
+        else if (w > 0 && h <= 0){
+        }
+
+        else if (w <= 0 && h > 0){
+        }
+
+        
+    }
+    print(w=this.width,h=this.height){
+        let i, j;
+        for (i=0; i<h; i++){
+            let row = '';
+            for(j=0;j<w;j++){
+                row += 'X';
+            }
+            console.log(row);
+        }
+    }
+    rotate(){
+        this.print(this.height, this.width);
+    }
+    double(){
+        this.print(this.width*2,this.height*2);
+    }
+
+};
+
+
+module.exports = Rectangle;
