8e24abec1b7726389cacaf846bfef527e52ee4bd
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-10-21 09:21:13 +0000

    Objects in Javascript

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -4,12 +4,8 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle{
     constructor(size){
         super(size, size);
-        this.size = size;
-        // console.log("Just inherited")
+        // this.size = size;
     }
 }
- 
-// const s1 = new Square(3);
-// s1.print()
 
 module.exports = Square;
