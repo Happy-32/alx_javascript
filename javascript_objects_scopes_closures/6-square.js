bbe10da785a86bea63fb453caf7c5eb6e1d5ec39
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-10-21 09:54:54 +0000

    Objects in Javascript

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,36 +1,27 @@
 #!/usr/bin/node
 const Square = require('./5-square');
 
-class Square1 extends Square{
-    constructor(size){
-        super(size);
-        // this.size = size;
-        // console.log("Just inherited")
+class Square1 extends Square {
+  constructor(size) {
+    super(size);
+  }
+
+  double(){
+    this.size = this.size*2;
+  }
+
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
     }
-    double(){
-        this.size = this.size*2;
+    for (let i = 0; i < this.size; i++) {
+      let row = '';
+      for (let j = 0; j < this.size; j++) {
+        row += c;
+      }
+      console.log(row);
     }
-    charPrint(c){
-        let i, j;
-        for (i=0; i<this.size; i++){
-            let row = '';
-            for(j=0;j<this.size;j++){
-                if (typeof c === 'undefined'){
-                    row += "X"
-                } else{
-                    row += c;
-                }
-            }
-            console.log(row);
-        }
-    }
+  }
 }
- 
-// const s1 = new Square(3);
-// s1.print()
 
 module.exports = Square1;
-
-
-
-module.exports = Square;
