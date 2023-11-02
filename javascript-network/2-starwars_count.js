716a5fd8da733a1d7d50c0d5704c973a5121f457
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-11-02 14:07:50 +0000

    Network

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -5,19 +5,19 @@ const id = 18;
 const url = process.argv[2];
 
 
-// request.get(url, function (error, response, body){
+request.get(url, function (error, response, body){
 
-//   try {
-//     const films = JSON.parse(body).results;
-//     const has_Wedge = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`));
-//     const numberofmovies = has_Wedge.length;
+  try {
+    const films = JSON.parse(body).results;
+    const has_Wedge = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${id}/`));
+    const numberofmovies = has_Wedge.length;
     
-//     console.log(numberofmovies);
-//   } catch (parseError){
-//     console.error('Error while parsing JSON: ', parseError);
-//   }
+    console.log(numberofmovies);
+  } catch (parseError){
+    console.error('Error while parsing JSON: ', parseError);
+  }
 
-// });
+});
 
 request.get(`https://swapi-api.alx-tools.com/api/people/${id}/`, {encoding: 'utf-8'}, function(error, response, body){
   try{
