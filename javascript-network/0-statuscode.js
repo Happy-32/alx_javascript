5ebfb06e0db0fbbd7e307763c8dba83925528fc1
Andrew Yahaya <yahayaenmaariandrew@gmail.com>
2023-11-02 13:31:36 +0000

    Network

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url, {encoding: 'utf8'}, function (error, response, body){
-console.log(response.statusCode)
+console.log("code: " + response.statusCode)
 });
